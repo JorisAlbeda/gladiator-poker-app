05e34452c53febf2afe339044649348185084988
JorisAlbeda <91875360+JorisAlbeda@users.noreply.github.com>
2025-03-17 17:14:14 +0100

    Created a battle page.

---
+++ pages/battle.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const players = useGetRealtimePlayers()
+</script>
+
+<template>
+  <div>
+    <UCard v-for="player in players" :key="player.id">
+      <div class="flex justify-between">
+        <span>{{ player.name }}</span>
+        <UButton>Hit</UButton>
+      </div>
+    </UCard>
+  </div>
+</template>
