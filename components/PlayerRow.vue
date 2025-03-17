67c1748ab8cb8c6ddc4e2dc599d15192808b8a1f
JorisAlbeda <91875360+JorisAlbeda@users.noreply.github.com>
2025-03-17 16:11:27 +0100

    Empty form after use.

--- components/PlayerRow.vue
+++ components/PlayerRow.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { FormSubmitEvent } from "@nuxt/ui"
+import type { Form, FormSubmitEvent } from "@nuxt/ui"
 import { z } from "zod"
 
 const schema = z.object({
@@ -34,11 +34,18 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
     })
   }
   formState.name = undefined
+  const form = useTemplateRef<Form<Schema>>("playerForm").value
+  form?.clear()
 }
 </script>
 
 <template>
-  <UForm :state="formState" :schema="schema" @submit.prevent="onSubmit">
+  <UForm
+    ref="playerForm"
+    :state="formState"
+    :schema="schema"
+    @submit.prevent="onSubmit"
+  >
     <div class="flex gap-4">
       <UFormField name="name">
         <UInput
