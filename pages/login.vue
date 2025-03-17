a937258c8670dd8c3fd77ed5c8cab3ef00c8f9ed
JorisAlbeda <91875360+JorisAlbeda@users.noreply.github.com>
2025-03-17 10:53:13 +0100

    Setup realtime database listener,

---
+++ pages/login.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type { RealtimeChannel } from "@supabase/supabase-js"
+
+const client = useSupabaseClient()
+
+let realtimeChannel: RealtimeChannel
+
+// Fetch players and get the refresh method provided by useAsyncData
+const { data: players, refresh: refreshPlayers } = await useAsyncData(
+  "players",
+  async () => {
+    const { data } = await client.from("players").select()
+    return data
+  }
+)
+
+// Once page is mounted, listen to changes on the `players` table and refresh players when receiving event
+onMounted(() => {
+  // Real time listener for new workouts
+  realtimeChannel = client
+    .channel("public:players")
+    .on(
+      "postgres_changes",
+      { event: "*", schema: "public", table: "players" },
+      () => refreshPlayers()
+    )
+
+  realtimeChannel.subscribe()
+})
+
+// Don't forget to unsubscribe when user left the page
+onUnmounted(() => {
+  client.removeChannel(realtimeChannel)
+})
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl mb-1">Login</h1>
+    <ul>
+      <li v-for="player in players" :key="player.id">
+        {{ player.name }}
+      </li>
+    </ul>
+  </div>
+</template>
